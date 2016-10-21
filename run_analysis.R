0216e12695d1f1594f11c05cf182ea57aaa6d795
lauraebaker <laura@brikk.co>
2016-10-21 12:34:50 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,16 @@
-#Load necessary packages
-library(dplyr)
 ## Don't forget to set your working directory: setwd()
+
+#Loads dependent packages
+library(dplyr)
   
-#Label the data set with descriptive variable names.
-  #read the feature and activity names into R
+#Reads the feature and activity names into R
   features<-read.table('./UCI_HAR_Dataset/features.txt',sep=" ")
   activity_labels<-read.table('./UCI_HAR_Dataset/activity_labels.txt',sep=" ",col.names=c("activity_id","activity"))
 
-#Define the column size for importing the fixed width x data
+#Defines the column size for importing the fixed-width x data
   fixedlenx<-rep(16,561)
   
-#Task 4: load the test and train data with variable names including activities and subjectids
+#Task 4: Reads the test and train data and assigns descriptive variable names including activities and subjectids
   xtest<-read.fwf('./UCI_HAR_Dataset/test/X_test.txt',fixedlenx,col.names=features$V2)
   ytest<-read.fwf('./UCI_HAR_Dataset/test/y_test.txt',2,col.names=c("activity_id"))
   subjtest<-read.fwf('./UCI_HAR_Dataset/test/subject_test.txt',2,col.names=c("subjectid"))
@@ -18,22 +18,22 @@ library(dplyr)
   ytrain<-read.fwf('./UCI_HAR_Dataset/train/y_train.txt',2,col.names=c("activity_id"))
   subjtrain<-read.fwf('./UCI_HAR_Dataset/train/subject_train.txt',2,col.names=c("subjectid"))
 
-#Task 1: Merge the training and the test sets to create one data set.  
+#Task 1: Merges the training and the test sets to create one data set.  
   allx<-rbind(xtest,xtrain)
   ally<-rbind(ytest,ytrain)
   allsubj<-rbind(subjtest,subjtrain)
-  
-#Merge subject and activity data with measurement data
-  alldata<-cbind(allsubj,ally,allx)
+    
+    #Merge subject and activity data with measurement data
+    alldata<-cbind(allsubj,ally,allx)
     
 #Task 2: Extracts the mean and standard deviation for each measurement.
   mnstddata<-alldata[,grep("[Mm]ean|std|id", names(alldata), value = TRUE)]
 
-#Task 3: Rename the activities in the data set using a more descriptive activity name   
+#Task 3: Adds the descriptive activity names to the dataset   
   activityfactors<-factor(mnstddata$activity_id,activity_labels$activity_id,labels=activity_labels$activity)
   mnstddatalabeled<-mutate(mnstddata,activity=activityfactors)
   
-#Task 5:Create a second dataset with the average of each variable from task 4 for each activity and each subject.
+#Task 5:Creates a dataset with the average of each variable by activity and subject.
   grpalldata<-group_by(mnstddatalabeled,subjectid,activity_id,activity)
   avgalldata<-summarise_each(grpalldata,funs(mean(.,na.rm=TRUE)))
   names(avgalldata)[4:89]<-paste("avg",names(avgalldata)[4:89],sep="_")
